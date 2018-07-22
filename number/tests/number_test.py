73154128f33d350909ce6925aa9b530a12c57051
Tamara Roberson <tamara.roberson@gmail.com>
2018-07-21 17:50:15 -0700

    Number: Change unit test function names
    * number/tests/number_test.py
    
    Make the unit test function names more descriptive about what exactly
    we're looking for.

--- number/tests/number_test.py
+++ number/tests/number_test.py
@@ -4,26 +4,26 @@ import bsdnumber.converter
 def test_zero():
 	assert bsdnumber.converter.number_to_string(0) == "zero"
 
-# from the one-part list
-def test_nineteen():
+# from the one-part list (1-19)
+def test_one_part():
 	assert bsdnumber.converter.number_to_string(19) == "nineteen"
 
-# from the two-part list with no remainder
-def test_twenty():
+# from the two-part list (20-99) with no remainder
+def test_two_parts_even():
 	assert bsdnumber.converter.number_to_string(20) == "twenty"
 
-# two-part number
-def test_twenty_five():
+# two-part number with ones digit
+def test_two_parts_both():
 	assert bsdnumber.converter.number_to_string(25) == "twenty-five"
 
-# three-part number, even
-def test_one_hundred():
+# three-part number (100-999), even
+def test_three_parts_even():
 	assert bsdnumber.converter.number_to_string(100) == "one hundred"
 
 # three-part number, first and last
-def test_one_hundred_eight():
+def test_three_parts_last():
 	assert bsdnumber.converter.number_to_string(108) == "one hundred eight"
 
 # three-part number, all digits
-def test_one_hundred_fifty_three():
+def test_three_parts_all():
 	assert bsdnumber.converter.number_to_string(153) == "one hundred fifty-three"
